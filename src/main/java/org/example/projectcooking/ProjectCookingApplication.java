3ce698d2a43d99bb7ee3650de7f7d415a4110e74
Danil <dkolbasov13@gmail.com>
2026-07-06 02:47:06 +0300

    Start of the Project

---
+++ src/main/java/org/example/projectcooking/ProjectCookingApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectcooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectcooking/ProjectCookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectcooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

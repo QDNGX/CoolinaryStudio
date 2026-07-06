0d3d22cfae81443098330fb3cccb4737d8dd55ba
Danil <dkolbasov13@gmail.com>
2026-07-06 10:15:26 +0300

    Move Spring Boot project into backend/ folder

--- backend/src/main/java/org/example/projectcooking/ProjectCookingApplication.java
+++ backend/src/main/java/org/example/projectcooking/ProjectCookingApplication.java
@@ -2,8 +2,10 @@ package org.example.projectcooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProjectCookingApplication {
 
     public static void main(String[] args) {
